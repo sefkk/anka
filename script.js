d3e09d1fdfa3de7684408ae88f249407591ac58b
Poyraz Temiz <99994089+PoyrazTemiz@users.noreply.github.com>
2025-03-09 00:34:17 +0100

    menü ikonu düzeltildi + komite scriptteki duplicate kod silindi

--- script.js
+++ script.js
@@ -227,5 +227,12 @@ document.addEventListener("click", function (event) {
     if (!menuToggle.contains(event.target) && menuOverlay.classList.contains("active")) {
         document.body.classList.remove("menu-active");
         menuOverlay.classList.remove("active");
+
+        // Menü ikonu eski haline döndürüyor
+        document.querySelectorAll(".hamburger div").forEach((bar, index) => {
+            bar.style.transform = document.body.classList.contains("menu")
+                ? `rotate(${index === 0 ? -45 : index === 2 ? 45 : 0}deg) translateY(${index === 1 ? 10 : 0}px)`
+                : "none";
+        });
     }
 });
