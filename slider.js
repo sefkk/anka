d4b4a026e3533863a7cf675df6bf04053bc8ee7b
orkun <orkunsefik@gmail.com>
2025-07-06 00:57:37 +0200

    prototype
    changed nearly everything

---
+++ slider.js
@@ -0,0 +1,69 @@
+// SLIDER 
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const newsSection = document.querySelector('.news-background');
+  const title = newsSection.querySelector('h2');
+  const shadow = newsSection.querySelector('.slider-shadow');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        newsSection.classList.add('animate');
+        title.classList.add('animate');
+        shadow.classList.add('animate');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.1,
+    rootMargin: '0px 0px -200px 0px'
+  });
+
+  observer.observe(newsSection);
+});
+
+// "WHY ANKA" ANIMATIONS
+
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.3,
+    rootMargin: "0px 0px 0px 0px"});
+  const animatedElements = document.querySelectorAll('.big-square, .text-area, .small-square');
+  animatedElements.forEach(el => observer.observe(el));
+});
+
+
