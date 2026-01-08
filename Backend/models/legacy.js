075ee209a820221af04d2cd036c8acdf1ad19dcc
Eda Cetinkaya <edacetinkaya06@yahoo.com>
2026-01-08 23:58:32 +0100

    Legacy subcategory eklenmesi

--- Backend/models/legacy.js
+++ Backend/models/legacy.js
@@ -9,6 +9,11 @@ const legacySchema = new mongoose.Schema({
     enum: ['board', 'vice-chair', 'committee'],
     required: true 
   },
+  subcategory: { 
+    type: String, 
+    enum: ['it', 'marketing', 'entrepreneurship', 'academic', 'event', 'hr', 'law', 'int-relations'],
+    required: false // Only required for committee category
+  },
   image: String, // Path to image
   linkedin: String, // LinkedIn URL
   createdAt: { type: Date, default: Date.now }
